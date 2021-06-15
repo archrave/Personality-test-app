65bb12f1350656256483ccec8e3f6b21e93f83f7
Divyansh Joshi <divyanshjoshi20@gmail.com>
2021-06-15 20:03:19 +0530

    Added some font and margin to the question

--- lib/question.dart
+++ lib/question.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
-
+//final tells dart that this value wouldnt change after the initialisation in the constructor (a good convention)
+  final String questionText;
   Question(this.questionText); //Short methoda of writing constructor
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(10),
+        child: Text(
+          questionText,
+          style: TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+        ));
   }
 }
